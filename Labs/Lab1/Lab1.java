d21fbb9aee87d47a1e947705c8f1b93f64741c20
Tushar Tyagi <tushar.tyagi.dxb@gmail.com>
2021-12-26 16:05:16 +0400

    Add files via upload

---
+++ Labs/Lab1/Lab1.java
@@ -0,0 +1,15 @@
+//***************************************************************
+//Name: Tushar Tyagi
+//Lab Letter: G
+//Title: Lab1.java
+//Description: Prints out my first program and my name
+//Date: 08/27/2021
+//***************************************************************
+public class Lab1
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("Hello, my name is Tushar.");
+		System.out.println("This is my first program!");
+	}
+}
